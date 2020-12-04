f3181e3a386833c380154914a2484220abbdf6cb
Sailytaskar1 <sailytaskar9@gmail.com>
2020-12-04 15:26:50 -0500

    First Commit

---
+++ src/test/java/tests/AppiumTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void Test1() {
+		
+		System.out.println("");
+		
+	}
+
+}
---
+++ src/test/java/tests/SeleniumTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void Test1() {
+		
+		System.out.println("TestCase1");
+		
+	}
+
+}
